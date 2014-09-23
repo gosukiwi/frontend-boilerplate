ea007ab46ec52a25cd7b6ec23b645725ce8eea9d
Federico Ramirez <fedekiller@gmail.com>
2014-09-23 08:48:57 -0300

    Add csscomb and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,24 @@ var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
+var csscomb = require('gulp-csscomb');
 
 var paths = {
   less: 'src/less/**/*.less',
   lessMain: 'src/less/style.less',
   scripts: 'src/js/**/*',
-  images: 'src/img/**/*',
-  html: 'src/*.html'
+  images: 'src/img/**/*.{jpg,png,gif}',
+  html: 'src/*.html',
+  vendor: 'src/vendor/**/*',
+  fonts: 'src/fonts/**/*'
 };
 
 gulp.task('scripts', function() {
   return gulp.src(paths.scripts)
     .pipe(concat('main.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('dist/static/js'))
     .pipe(reload({ stream: true }));
 });
 
@@ -36,7 +40,21 @@ gulp.task('images', function() {
   return gulp.src(paths.images)
     // Pass in options to the task
     .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest('dist/img'))
+    .pipe(gulp.dest('dist/static/img'))
+    .pipe(reload({ stream: true }));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src(paths.fonts)
+    // Pass in options to the task
+    .pipe(gulp.dest('dist/static/fonts'))
+    .pipe(reload({ stream: true }));
+});
+
+gulp.task('vendor', function() {
+  return gulp.src(paths.vendor)
+    // Pass in options to the task
+    .pipe(gulp.dest('dist/static/vendor'))
     .pipe(reload({ stream: true }));
 });
 
@@ -44,7 +62,17 @@ gulp.task('less', function() {
   return gulp.src(paths.lessMain)
     // Pass in options to the task
     .pipe(less())
-    .pipe(gulp.dest('dist/css'))
+    .pipe(autoprefixer([
+      'Android 2.3',
+      'Android >= 4',
+      'Chrome >= 20',
+      'Firefox >= 24', // Firefox 24 is the latest ESR
+      'Explorer >= 8',
+      'iOS >= 6',
+      'Opera >= 12',
+      'Safari >= 6']))
+    .pipe(csscomb())
+    .pipe(gulp.dest('dist'))
     .pipe(reload({ stream: true }));
 });
 
@@ -52,15 +80,18 @@ gulp.task('less', function() {
 gulp.task('watch', function() {
   browserSync({
     server: {
-      baseDir: 'dist'
+      baseDir: 'dist'/*,
+      host: '0.0.0.0'*/
     }
   });
 
   gulp.watch(paths.less, ['less']);
   gulp.watch(paths.html, ['html']);
+  gulp.watch(paths.fonts, ['fonts']);
   gulp.watch(paths.images, ['images']);
+  gulp.watch(paths.vendor, ['vendor']);
   gulp.watch(paths.scripts, ['scripts']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'less', 'html', 'scripts', 'images']);
+gulp.task('default', ['watch', 'less', 'fonts', 'vendor', 'html', 'scripts', 'images']);
